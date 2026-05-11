dcc4cd31efd41db6cd582bf0deb44d122628d5fe
Brindha R <rbrindha242@gmail.com>
2026-05-11 12:31:09 +0530

    Update Location.js
    coded

--- src/backend-api/models/Location.js
+++ src/backend-api/models/Location.js
@@ -1,1 +1,9 @@
 
+const mongoose = require("mongoose");
+
+const locationSchema = new mongoose.Schema({
+  id: String,
+  name: String
+});
+
+module.exports = mongoose.model("Location", locationSchema);
