85f92d5d3e7fb470944d3044adf92079ca46bd13
Brindha R <rbrindha242@gmail.com>
2026-05-11 17:01:11 +0530

    Update server.js
    updated

--- src/backend-api/server.js
+++ src/backend-api/server.js
@@ -10,6 +10,8 @@ const contactRoutes = require("./routes/contact");
 
 app.use("/contact", contactRoutes);
 
-app.listen(3000, () => {
-  console.log("Server running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
